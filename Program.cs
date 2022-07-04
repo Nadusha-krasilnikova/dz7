0c7e6c3645da48d2efddaf6cba76f3389b7ce5ac
Nadusha <nadusha230275@gmail.com>
2022-07-04 21:04:09 +0500

    работает

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// выясните кратно ли число заданному, если нет, вывести остаток
+Console.WriteLine("введите число");
+int a=int.Parse(Console.ReadLine());
+int b=4;
+if (a % b==0)
+{
+    Console.WriteLine("кратно числу");
+}
+else
+{
+    Console.WriteLine("число не кратно, остаток от деления =");
+    Console.WriteLine(a % b);
+}
